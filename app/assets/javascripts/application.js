6554b723740b0cf955dd410221eb909c0f59d1b1
ikhakoo <ikhakoo@gmail.com>
2015-05-07 19:56:15 -0400

    updating a lot of styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
 //= require shoppe/stripe/form_handler
+
+ 
---
+++ app/models/shoppe/product_decorator.rb
@@ -0,0 +1,9 @@
+
+Shoppe::Product.class_eval do
+
+	def testmeth
+		puts "Testing"
+	end
+
+end
+
