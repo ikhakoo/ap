577f519e442fa46232df8cf3d3f89b680f70f7f4
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-06-10 15:00:17 -0400

    fixed the stock_control problem with adding items to your basket

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
 
   def buy
 	  @product = Shoppe::Product.find_by_permalink!(params[:permalink])
-    if @product.stock_control = 'false'
+    if @product.stock_control == 'false'
       redirect_to product_path(@product.permalink), :notice => "Sorry we are out of stock!"
     else  
   	  current_order.order_items.add_item(@product, 1)
