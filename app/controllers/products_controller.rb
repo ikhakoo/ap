7f9ce85b129ab3976b05ea32e2eca3b5b3d1b27b
ikhakoo <ikhakoo@gmail.com>
2015-06-15 00:59:30 -0400

    updating controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,11 +11,12 @@ class ProductsController < ApplicationController
 
   def buy
 	  @product = Shoppe::Product.find_by_permalink!(params[:permalink])
-    if @product.stock_control = 'false'
-      redirect_to product_path(@product.permalink), :notice => "Sorry we are out of stock!"
+    if @product.stock_control = 'true'
+      current_order.order_items.add_item(@product, 1)
+      redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
     else  
-  	  current_order.order_items.add_item(@product, 1)
-  	  redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
+      binding.pry
+      redirect_to product_path(@product.permalink), :notice => "Sorry we are out of stock!"
     end
 	end
 
