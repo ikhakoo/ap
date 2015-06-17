be0a76d67324ac4df35578bc59f45afcd9394e59
ikhakoo <ikhakoo@gmail.com>
2015-06-17 12:50:39 -0400

    fixed conflict with products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,8 +11,9 @@ class ProductsController < ApplicationController
 
   def buy
 	  @product = Shoppe::Product.find_by_permalink!(params[:permalink])
-    if @product.stock_control == 'false'
-      redirect_to product_path(@product.permalink), :notice => "Sorry we are out of stock!"
+    if @product.stock_control = 'true'
+      current_order.order_items.add_item(@product, 1)
+      redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
     else  
       redirect_to product_path(@product.permalink), :alert => "Sorry we are out of stock!"
     end
