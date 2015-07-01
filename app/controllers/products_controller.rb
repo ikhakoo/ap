0a99c52ddbfcbcec9304fa5b850f004af220b9bd
ikhakoo <ikhakoo@gmail.com>
2015-06-30 21:29:09 -0400

    removing unused code from controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -125,16 +125,6 @@ class ProductsController < ApplicationController
     
   end
 
-  # def buy
-  #   @product = Shoppe::Product.find_by_permalink!(params[:permalink])
-  #   if @product.stock_control # do we have any stock left?
-  #     current_order.order_items.add_item(@product, 1)
-  #     redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
-  #   else  
-  #     redirect_to product_path(@product.permalink), :alert => "Sorry we are out of stock!"
-  #   end
-  # end
-
   def buy
     @product = Shoppe::Product.find(params[:product].to_i)
     if params[:variant]
