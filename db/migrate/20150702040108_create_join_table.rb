1c055e7dcf2055bf2773db41c55cfc89f2f5d7c7
ikhakoo <ikhakoo@gmail.com>
2015-07-02 01:01:14 -0400

    removed join table

--- db/migrate/20150702040108_create_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTable < ActiveRecord::Migration
-  def change
-    create_join_table :clients, :shoppe_orders do |t|
-      t.index [:client_id, :shoppe_order_id]
-      t.index [:shoppe_order_id, :client_id]
-    end
-  end
-end
