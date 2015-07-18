8b9571f1fae741d9c2b708926961080271bae4ef
ikhakoo <ikhakoo@gmail.com>
2015-07-18 17:19:37 -0400

    link fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,7 @@ def seed_shit
 			detail line across the chest and two tailored lines down the back. Two lower patch pockets,
 			the right side is enclosed with inner sectional pockets.The top is finished off with two small side 
 			slits and a convenient shoulder pen pocket.</p><p>Stretch-Flex Blend: 65% Poly/32% Rayon/3% Spandex</p>",
-			colors: ["Black", "Royal Blue", "Charcoal", "White"],
+			colors: ["Black", "RoyalBlue", "Charcoal", "White"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
 		"The Pearl.jpeg" => {
@@ -59,7 +59,7 @@ def seed_shit
 			pocket and two lower patch pockets. One of the patch pockets includes detailed stitching on the 
 			outside with a utility D-ring and an inner smart phone pocket.</p><p>*Stretch-Flex Blend: 
 			65% Poly/32% Rayon/3% Spandex</p>",
-		  colors: ["Black", "Royal Blue", "Charcoal", "White"],
+		  colors: ["Black", "RoyalBlue", "Charcoal", "White"],
 		  sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
 		"Basic V Neck Scrub Top.jpeg" => {
@@ -68,25 +68,25 @@ def seed_shit
 			simplicity. Featuring a standard v-neck with one chest pocket and one shoulder pen pocket. This top 
 			mates perfectly with 608P and is a favorite among school programs.</p>",
 			colors: ["Black", "Cappuccino", "Ceil", 
-								"Lagoon", "Postman Blue", "Sky Blue", 
+								"Lagoon", "PostmanBlue", "SkyBlue", 
 								"Burgundy", "Caribbean", "Charcoal", 
-								"Navy Blue", "Royal Blue", "White"
+								"NavyBlue", "RoyalBlue", "White"
 							],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"]
 		},
 		"Zipper Front Ladies Work Top.jpeg" => {
 			sku: "202T",
 			description: "<p>Zip up in this ladies work top featuring 2 patch pockets, a zip front closure and a collar</p>",
-			colors: ["Black", "White", "Navy Blue"],
+			colors: ["Black", "White", "NavyBlue"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
 		"Flexi V Neck Scrub Top.jpeg" => {
 			sku: "324T",
 			description: "<p>Zip up in this ladies work top featuring 2 patch pockets, a zip front closure and a collar</p>",
-			colors: ["2tone White/White", "Black", "Brown", "Charcoal", "Deja Vu", "Eggplant", "Heart Ties", "Khaki",
-							 "Lime Green", "Olive Green", "Papillon", "Postman Blue", "Raspberry", "Royal Blue", "String Along",
-							 "Zen Stem", "Aqua", "Blue Cheerios", "Cappuccino", "Deep Orchid", "Dusty Rose", "Fire Works", "Indigo",
-							 "Lagoon", "Navy Blue", "Paisley Pink", "Pink Sorbet", "Pretty in Petals", "Red", "Sea Green",
+			colors: ["2toneWhite/White", "Black", "Brown", "Charcoal", "DejaVu", "Eggplant", "HeartTies", "Khaki",
+							 "LimeGreen", "OliveGreen", "Papillon", "PostmanBlue", "Raspberry", "RoyalBlue", "StringAlong",
+							 "ZenStem", "Aqua", "BlueCheerios", "Cappuccino", "DeepOrchid", "DustyRose", "FireWorks", "Indigo",
+							 "Lagoon", "NavyBlue", "PaisleyPink", "PinkSorbet", "PrettyInPetals", "Red", "SeaGreen",
 							 "Techno"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
@@ -94,15 +94,15 @@ def seed_shit
 			sku: "524T",
 			description: "<p>The Ultra Flexi scrub top has multiple black stretch panels for the most superb fit 
 			and ultimate movement.The Ultra Flexi v-neck top also has two lower pockets and one shoulder pen pocket.</p>",
-			colors: ["Aqua", "Charcoal", "Navy Blue", "Black", "Deep Orchid", "Sky Blue"],
+			colors: ["Aqua", "Charcoal", "NavyBlue", "Black", "DeepOrchid", "SkyBlue"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"3 Pocket V Neck Scrub Top.jpeg" => {
 			sku: "308T",
 			description: "<p>A bold, basic 3 pocket v-neck scrub top featuring dolman sleeves. This scrub top is also available as a set</p>",
 			colors: ["Aqua", "Burgundy", "Ceil", "Eggplant", "Lagoon", "Postman Blue", 
-								"Royal Blue", "Spruce", "Black", "Caribbean", "Charcoal", "Khaki", 
-								"Navy Blue", "Rasberry", "Sky Blue", "White"],
+								"RoyalBlue", "Spruce", "Black", "Caribbean", "Charcoal", "Khaki", 
+								"NavyBlue", "Rasberry", "SkyBlue", "White"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL"]
 		},
 		"Unisex V Neck Scrub Top.jpeg" => {
@@ -110,32 +110,32 @@ def seed_shit
 			description: "<p>A classic v-neck scrub top popular among men and women. This top has a slightly longer hem 
 			line making it an excellent choice for anyone with a taller stature. This top has three front pockets, one shoulder 
 			pen pocket and is available in every MOBB color</p>",
-			colors: ["Aqua", "Burgundy", "Caribbean", "Charcoal", "Khaki", "Navy Blue", "Postman Blue", "Royal Blue", 
-								"Sky Blue", "Teal", "Black", "Cappuccino", "Ceil", "Eggplant", "Lagoon", "Olive Green", "Red",
-								"Sea Green", "Spruce", "White"],
+			colors: ["Aqua", "Burgundy", "Caribbean", "Charcoal", "Khaki", "NavyBlue", "PostmanBlue", "RoyalBlue", 
+								"SkyBlue", "Teal", "Black", "Cappuccino", "Ceil", "Eggplant", "Lagoon", "OliveGreen", "Red",
+								"SeaGreen", "Spruce", "White"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"]
 		},
 		"Ladies Sculpted Scrub Top.jpeg" => {
 			sku: "410T",
 			description: "<p>A simple twist on our classic neckline. This scrub top features a beautifully sculpted wave neckline 
 			with contrasting color. The top also has two lower pockets, one chest pocket and one shoulder pen pocket.</p>",
-			colors: ["Aqua/Black", "Charcoal/Postman Blue", "Eggplant/Black", "White/White", 
-								"Black/White", "Deep Orchid/White", "Navy Blue/Sky Blue"],
+			colors: ["Aqua/Black", "Charcoal/PostmanBlue", "Eggplant/Black", "White/White", 
+								"Black/White", "DeepOrchid/White", "NavyBlue/Sky Blue"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"Active Flexi V Neck Scrub Top.jpeg" => {
 			sku: "424T",
 			description: "<p>Comfortable and stylish this scrub top feels and looks amazing. The stretch panels create a fantastic fit 
 			and allow for great movement. The flexi v-neck has two lower pockets and one shoulder pen pocket</p>",
-			colors: ["Indigo/Black", "Eggplant/Black", "Rasberry/Black", "Aqua/Black", "Charcoal/Black", "Navy Blue/Black",
-								"White/White", "Postman Blue/Black"],
+			colors: ["Indigo/Black", "Eggplant/Black", "Rasberry/Black", "Aqua/Black", "Charcoal/Black", "NavyBlue/Black",
+								"White/White", "PostmanBlue/Black"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"Mens Two Tone Scrub Top.jpeg" => {
 			sku: "409T",
 			description: "<p>Available in four color-ways this scrub top is becoming a favorite among men in the medical field. 
 			This two-tone scrub top has one chest pocket and one shoulder pen pocket. Also available as a set with the 409P</p>",
-			colors: ["Burgundy/Navy", "Khaki/Black", "Charcoal/Black", "Postman Blue/Navy"],
+			colors: ["Burgundy/Navy", "Khaki/Black", "Charcoal/Black", "PostmanBlue/Navy"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
 		"Empire Tie Back Scrub Top.jpeg" => {
@@ -148,57 +148,57 @@ def seed_shit
 			sku: "T1018",
 			description: "<p>A stunning v-neck top with an hourglass silhouette. Featuring flex sleeves and side panels with a gathered cap sleeve detail, 
 			two hidden pockets and custom tailoring for an exceptional fit.</p><p>*Hy-Stretch Blend: 65% Poly/32% Cotton/3% Spandex<p>",
-			colors: ["Aubergine", "White", "Soft Mint"],
+			colors: ["Aubergine", "White", "SoftMint"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"The Mandy.jpeg" => {
 			sku: "T1010",
 			description: "<p>A unique style and ultra chic fit, this deep cut mandarin neckline features two lower pockets, flex side panels and has 
 			beautiful tailoring through the back for a custom fit.</p><p>*Hy-Stretch Blend: 65% Poly/32% Cotton/3% Spandex</p>",
-			colors: ["Aubergine", "White", "Soft Mint"],
+			colors: ["Aubergine", "White", "SoftMint"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"The Roxy.jpeg" => {
 			sku: "T1016",
 			description: "<p>A fit for fashion and function, this top features flex sleeves and side panels, a gathered cap sleeve detail, 
 			chest darts and two hidden pockets.</p><p>*Hy-Stretch Blend: 65% Poly/32% Cotton/3% Spandex</p>",
-			colors: ["Aubergine", "White", "Soft Mint"],
+			colors: ["Aubergine", "White", "SoftMint"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"Ladies Zipper Detail Scrub Top.jpeg" => {
 			sku: "530T",
 			description: "<p>Fun, funky and fitted. This sporty scrub top features reflective trim, asymmetrical zipper detail, two lower pockets, one chest pocket and one shoulder pen pocket</p>",
-			colors: ["Navy Blue"],
+			colors: ["NavyBlue"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
 		"V Neck Print Scrub Top.jpeg" => {
 			sku: "320T",
 			description: "<p>This classic printed v-neck scrub top is great for those who need to carry as many accessories as possible. A total of 5 front pockets offer a home for everything you need to carry throughout the day</p>",
-			colors: ["Blue Cheerios", "Fire Works", "Flower Power", "Heart Ties", "Pinned Heart", "Spring Blooming", 
-								"Techno", "Dizzy Vines", "Florescent Butterfly", "Groovy Hoops", "Papillon", "Pretty in Petals",
-								"String Along"],
+			colors: ["BlueCheerios", "FireWorks", "FlowerPower", "HeartTies", "PinnedHeart", "SpringBlooming", 
+								"Techno", "DizzyVines", "FlorescentButterfly", "GroovyHoops", "Papillon", "PrettyInPetals",
+								"StringAlong"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
 		},
 		"Criss Cross Scrub Top.jpeg" => {
 			sku: "323T",
 			description: "<p>Available in a large variety of prints and solid colors this scrub top will always be a top seller. Features a v-neck with a contrast color trim as well as two lower pockets and one shoulder pen pocket. This scrub top is also available as a set</p>",
-			colors: ["Aqua/Black", "Blue Cheerios/Navy Blue", "Charcoal/Postman Blue", "Eggplant/Black", "Floresent Butterfly/Charcoal",
-								"Lagoon/Lagoon", "Postman Blue/Navy", "Sea Green/Navy", "Spring Blooming/Black" , "Techno/Aqua", 
-								"Black/Aqua", "Charcoal/Lilac", "Deep Orchid/Black", "Fireworks/Sea Green", "Flower Power/Royal Blue", 
-								"Indigo/Charcoal", "Navy Blue/Sky Blue", "Pretty in Petals/Charcoal", "Sky Blue/Navy", 
-								"String Along/Eggplant", "White/White"],
+			colors: ["Aqua/Black", "BlueCheerios/NavyBlue", "Charcoal/PostmanBlue", "Eggplant/Black", "FloresentButterfly/Charcoal",
+								"Lagoon/Lagoon", "Postman Blue/Navy", "Sea Green/Navy", "SpringBlooming/Black" , "Techno/Aqua", 
+								"Black/Aqua", "Charcoal/Lilac", "Deep Orchid/Black", "Fireworks/SeaGreen", "FlowerPower/RoyalBlue", 
+								"Indigo/Charcoal", "NavyBlue/SkyBlue", "PrettyInPetals/Charcoal", "Sky Blue/Navy", 
+								"StringAlong/Eggplant", "White/White"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL"]
 		},
 		"Button Front Ladies Work Top.jpeg" => {
 			sku: "201T",
 			description: "<p>Button up in this ladies work top featuring 3 patch pockets, button front closure and a collar</p>",
-			colors: ["Black", "White", "Navy Blue"],
+			colors: ["Black", "White", "NavyBlue"],
 			sizes: ["S", "M", "L", "XL", "2XL"]
 		},
 		"Ladies Long Sleeve Tee.jpeg" => {
 			sku: "LT101",
 			description: "<p>100% cotton, 100% comfort. This ladies long sleeve tee is made with pre-shrunk cotton and is perfect worn on it's own or layered under a scrub top. The top stitched collar gives it the strength and durability MOBB is known for</p>",
-			colors: ["Black", "White", "Navy Blue"],
+			colors: ["Black", "White", "NavyBlue"],
 			sizes: ["S", "M", "L", "XL", "2XL"]
 		}
 	}
@@ -209,15 +209,15 @@ def seed_shit
 			description: "<p>Amazing fit in every MOBB color imaginable. Unisex v-neck scrub top with 3 front pockets and one shoulder pen pocket and a 5 pocket scrub pant with a combination drawstring and elastic waist for maximum comfort.<p>
 <p><strong>Size 5XL only available in Colors: Black, Charcoal, Burgundy, Caribbean, Lagoon, Navy, Postman Blue, Royal Blue</p></strong>
 <p><strong>Size 6XL only available in Navy</p></strong>",
-			colors: ["Aqua","Burgundy","Caribbean","Charcoal","Khaki","Lime Green","Olive Green","Royal Blue","Spruce",
-								"White","Black","Cappuccino","Ceil","Eggplant","Lagoon","Navy Blue","Postman Blue","Sky Blue","Teal"],
+			colors: ["Aqua","Burgundy","Caribbean","Charcoal","Khaki","LimeGreen","OliveGreen","RoyalBlue","Spruce",
+								"White","Black","Cappuccino","Ceil","Eggplant","Lagoon","NavyBlue","PostmanBlue","SkyBlue","Teal"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"]
 		},
 		"Unisex 5 Pocket Drawstring Scrub Set.jpeg" => {
 			sku: "606/608",
 			description: "<p>MOBB's newest style, this scrub set replaces the 306/306. A classic v-neck scrub top with one chest pocket and one shoulder pen pocket and a simple five pocket drawstring pant. This set is ideal for school programs.</p>",
 			colors: ["Black","Cappuccino","Ceil","Lagoon","Postman Blue","Sky Blue","Burgundy","Caribbean","Charcoal",
-								"Navy Blue","Royal Blue","White"],
+								"NavyBlue","RoyalBlue","White"],
 			sizes: ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"]
 		},
 		"Reversible O.R. Drawstring Scrub Set.jpeg" => {
