b853d680296690c9fdbf68c23f5544950a3b00d6
ikhakoo <ikhakoo@gmail.com>
2015-07-19 01:20:03 -0400

    updating seeds file and index items

--- db/seeds.rb
+++ db/seeds.rb
@@ -553,7 +553,7 @@ def seed_shit
 				params[:colors].each do |color|
 
 				# pro = Shoppe::Product.new(:name => name, :sku => sku, :description => 'test', :short_description => 'test', :weight => 1.119, :price => 24.99, :cost_price => 8.99, :tax_rate => tax_rate)
-				pro = Shoppe::Product.new(default_params.merge(description: params[:description], sku: params[:sku], name: "#{name}c-#{color}"))
+				pro = Shoppe::Product.new(default_params.merge(description: params[:description], sku: params[:sku], name: "#{name}-CLEARANCE-#{color}"))
 				pro.product_category = cat6
 				pro.default_image_file = get_file(filename)
 				pro.save!
