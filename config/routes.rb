b975f2ed450dc63437117c3e6c8cfd64f43b533a
ikhakoo <ikhakoo@gmail.com>
2015-07-31 00:09:15 -0400

    updating route names

--- config/routes.rb
+++ config/routes.rb
@@ -11,42 +11,42 @@ Rails.application.routes.draw do
   match "checkout", to: "orders#checkout", as: "checkout", via: [:get, :patch]
   match "checkout/pay", to: "orders#payment", as: "checkout_payment", via: [:get, :post]
   match "checkout/confirm", to: "orders#confirmation", as: "checkout_confirmation", via: [:get, :post]
-  match "my_orders", to: "orders#my_orders", as: "my_orders", via: [:get]
+  match "my-orders", to: "orders#my_orders", as: "my_orders", via: [:get]
 
   match "remove_item", to: "orders#remove_item", as: "remove_item", via: [:get]
   match "increase_item_quantity", to: "orders#increase_item_quantity", as: "increase_item_quantity", via: [:get]
   match "decrease_item_quantity", to: "orders#decrease_item_quantity", as: "decrease_item_quantity", via: [:get]
 
   #Nurse Wear
-  match "nurse_tops", to: "products#nurse_tops", as: "nurse_tops", via: [:get]
-  match "nurse_sets", to: "products#nurse_sets", as: "nurse_sets", via: [:get]
-  match "nurse_coats", to: "products#nurse_sets", as: "nurse_coats", via: [:get]
-  match "nurse_pants", to: "products#nurse_sets", as: "nurse_pants", via: [:get]
+  match "nurse/tops", to: "products#nurse_tops", as: "nurse_tops", via: [:get]
+  match "nurse/sets", to: "products#nurse_sets", as: "nurse_sets", via: [:get]
+  match "nurse/coats", to: "products#nurse_sets", as: "nurse_coats", via: [:get]
+  match "nurse/pants", to: "products#nurse_sets", as: "nurse_pants", via: [:get]
   match "accessories", to: "products#accessories", as: "accessories", via: [:get]
-  match "nurse_caps", to: "products#nurse_caps", as: "nurse_caps", via: [:get]
+  match "nurse/caps", to: "products#nurse_caps", as: "nurse_caps", via: [:get]
   
   #Mentality
-  match "mentality_stretchflex", to: "products#mentality_stretchflex", as: "mentality_stretchflex", via: [:get]
-  match "mentality_hyflex", to: "products#mentality_hyflex", as: "mentality_hyflex", via: [:get]
+  match "mentality/stretchflex", to: "products#mentality_stretchflex", as: "mentality_stretchflex", via: [:get]
+  match "mentality/hyflex", to: "products#mentality_hyflex", as: "mentality_hyflex", via: [:get]
 
   #Stethoscopes
   match "stethoscopes", to: "products#stethoscopes", as: "stethoscopes", via: [:get]
 
   #Chef Wear
-  match "chef_coats", to: "products#chef_coats", as: "chef_coats", via: [:get]
-  match "chef_pants", to: "products#chef_pants", as: "chef_pants", via: [:get]
-  match "chef_hats", to: "products#chef_hats", as: "chef_hats", via: [:get]
-  match "chef_tops", to: "products#chef_tops", as: "chef_tops", via: [:get]
+  match "chef/coats", to: "products#chef_coats", as: "chef_coats", via: [:get]
+  match "chef/pants", to: "products#chef_pants", as: "chef_pants", via: [:get]
+  match "chef/hats", to: "products#chef_hats", as: "chef_hats", via: [:get]
+  match "chef/tops", to: "products#chef_tops", as: "chef_tops", via: [:get]
   match "aprons", to: "products#aprons", as: "aprons", via: [:get]
   match "waitress_vests", to: "products#waitress_vests", as: "waitress_vests", via: [:get]
 
   #work wear
-  match "work_fire_retardent", to: "products#work_fire_retardent", as: "work_fire_retardent", via: [:get]
-  match "work_shop_coat", to: "products#work_shop_coat", as: "work_shop_coat", via: [:get]
-  match "work_coverall_overall", to: "products#work_coverall_overall", as: "work_coverall_overall", via: [:get]
-  match "work_pants", to: "products#work_pants", as: "work_pants", via: [:get]
-  match "work_shirts", to: "products#work_shirts", as: "work_shirts", via: [:get]
-  match "work_vests", to: "products#work_vests", as: "work_vests", via: [:get]
+  match "work-wear/fire-retardent", to: "products#work_fire_retardent", as: "work_fire_retardent", via: [:get]
+  match "work-wear/shop-coat", to: "products#work_shop_coat", as: "work_shop_coat", via: [:get]
+  match "work-wear/coverall-overall", to: "products#work_coverall_overall", as: "work_coverall_overall", via: [:get]
+  match "work-wear/pants", to: "products#work_pants", as: "work_pants", via: [:get]
+  match "work-wear/shirts", to: "products#work_shirts", as: "work_shirts", via: [:get]
+  match "work-wear/safety-vests", to: "products#work_vests", as: "work_vests", via: [:get]
 
   #clearance
   match "clearance", to: "products#clearance", as: "clearance", via: [:get]
