e9660b5c595125185c238b3fb97d922499621be1
ikhakoo <ikhakoo@gmail.com>
2015-08-05 21:08:33 -0400

    fixing search issue

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
 
 	def index
-		@products = Shoppe::Product.select("DISTINCT ON (shoppe_products.sku) shoppe_products.*").where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%").page(params[:page]).per(6)
+		@products = Shoppe::Product.select("DISTINCT ON (shoppe_products.sku) shoppe_products.*")
+		@products = @products.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%").page(params[:page]).per(6)
 	end
 
 end
