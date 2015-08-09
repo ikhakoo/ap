f2710d55d28a336894adfebc2d69a883922c14ab
ikhakoo <ikhakoo@gmail.com>
2015-08-09 01:48:04 -0400

    add sku to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
 	def index
 		@products = Shoppe::Product.all
-		@products = @products.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%").page(params[:page]).per(8)
+		@products = @products.where("LOWER(name) LIKE LOWER(?) OR LOWER(sku) LIKE LOWER(?)", "%#{params[:search]}%", "%#{params[:search]}%").page(params[:page]).per(8)
 	end
 
 end
