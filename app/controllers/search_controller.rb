069828d050abe720a4aea2062e010cbf1dd2437c
ikhakoo <ikhakoo@gmail.com>
2015-08-09 19:46:30 -0400

    adding items per page to search and updating show page left column products

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,8 @@ class SearchController < ApplicationController
 
 	def index
 		@products = Shoppe::Product.all
-		@products = @products.where("LOWER(name) LIKE LOWER(?) OR LOWER(sku) LIKE LOWER(?)", "%#{params[:search]}%", "%#{params[:search]}%").page(params[:page]).per(8)
+		@per_page = params[:per_page] || 8
+		@products = @products.where("LOWER(name) LIKE LOWER(?) OR LOWER(sku) LIKE LOWER(?)", "%#{params[:search]}%", "%#{params[:search]}%").page(params[:page]).per(@per_page)
 	end
 
 end
