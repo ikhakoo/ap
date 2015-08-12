2bbc6ed88856cd6cd2fef5904e45d21ea133103a
ikhakoo <ikhakoo@gmail.com>
2015-08-12 19:31:09 -0400

    adding route for gowns

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   match "nurse-wear/pants", to: "products#nurse_pants", as: "nurse_pants", via: [:get]
   match "accessories", to: "products#accessories", as: "accessories", via: [:get]
   match "nurse-wear/caps", to: "products#nurse_caps", as: "nurse_caps", via: [:get]
+  match "nurse-wear/gowns", to: "products#nurse_gowns", as: "nurse_gowns", via: [:get]
 
   resources :contact_forms
   
