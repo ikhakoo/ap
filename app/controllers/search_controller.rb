a85f235366597cb098b699d1bc4bac8341fd52e9
ikhakoo <ikhakoo@gmail.com>
2015-08-14 15:20:03 -0400

    update search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
 		@products = Shoppe::Product.all
 		@per_page = params[:per_page] || 8
 		@products = @products.where("LOWER(name) LIKE LOWER(?) OR LOWER(sku) LIKE LOWER(?)", "%#{params[:search]}%", "%#{params[:search]}%").page(params[:page]).per(@per_page)
+		@prods    = Shoppe::Product.all
 	end
 
 end
