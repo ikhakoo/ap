31ae25cc186c7fbe4fde4a33b7caedea06c273ce
ikhakoo <ikhakoo@gmail.com>
2015-10-15 21:54:00 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { :host => 'apuniforms.herokuapp.com' }
+  config.action_mailer.default_url_options :host => 'apuniforms.herokuapp.com'
 ​
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
