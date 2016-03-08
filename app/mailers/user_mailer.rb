c0ed689a53f6ddb0fbb52f6a8a1fc6f1a8f288e0
Daniel Dvorkin <dvorkin212@gmail.com>
2016-03-08 12:24:54 -0500

    updating branch

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
-	
+
 	def send_order_confirmation(order, client)
   	@order = order
   	@client = client
   	mail from: Shoppe.settings.outbound_email_address, to: @client.email, subject: "AP Uniforms Order Confirmation"
   end
+
 
 end
