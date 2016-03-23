d1cd9f3b4140290b00e731f69234cc489b704010
Daniel Dvorkin <Dvorkin212@gmail.com>
2016-03-22 23:29:57 -0400

    might have fixed the mailer issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,15 @@
-class UserMailer < ApplicationMailer
+module Shoppe
+	class UserMailer < NotificationMailer
+		def send_order_confirmation(order, client)
+	  	@order = order
+	  	@client = client
+	  	mail from: Shoppe.settings.outbound_email_address, to: @client.email, subject: "AP Uniforms Order Confirmation"
+	  end
 
-	def send_order_confirmation(order, client)
-  	@order = order
-  	@client = client
-  	mail from: Shoppe.settings.outbound_email_address, to: @client.email, subject: "AP Uniforms Order Confirmation"
-  end
-
-  def send_admin_notification(order, client)
-  	@order = order
-  	@client = client
-  	mail from: Shoppe.settings.outbound_email_address, to: "apuniforms@gmail.com", subject: "New Order from: #{@client.email}"
-  end
-
-
+	  def send_admin_notification(order, client)
+	  	@order = order
+	  	@client = client
+	  	mail from: Shoppe.settings.outbound_email_address, to: "apuniforms@gmail.com", subject: "New Order from: #{@client.email}"
+	  end
+	end
 end
