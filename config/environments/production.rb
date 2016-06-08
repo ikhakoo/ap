2e9e6cb745f6e56f51a0ac12956b9dee941c28fc
Daniel Dvorkin <Dvorkin212@gmail.com>
2016-06-08 19:53:17 -0400

    Fixed the production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'apuniforms.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
+    address: ‘smtpout.secureserver.net’,
+    port: 80,
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
